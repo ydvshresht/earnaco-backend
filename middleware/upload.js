c65f1edf5f93b844d0120723bd146d1b1b3a137f
Shubham Yadav <sy978060@gmail.com>
2026-02-04 21:30:59 +0530

    update backend

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,14 +6,22 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "earnaco/profiles",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"]
+   allowed_formats: ["jpg", "jpeg", "png", "webp", "heic", "heif"]
+
   }
 });
 
 const fileFilter = (_, file, cb) => {
-  if (file.mimetype.startsWith("image/")) cb(null, true);
-  else cb(new Error("Only images allowed"));
+  if (
+    file.mimetype.startsWith("image/") ||
+    ["image/heic", "image/heif"].includes(file.mimetype)
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images allowed"));
+  }
 };
+
 
 module.exports = multer({
   storage,
